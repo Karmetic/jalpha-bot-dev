44a6a82bd09567801daf03dc0fed519fabc5e154
Ian Fields <32248377+Karmetic@users.noreply.github.com>
2023-03-15 15:08:53 -0500

    Bot Creation and Connection

---
+++ commands/install-sex-mod.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('install-sex-mod')
+    .setDescription('Holden needed this')
+    ,
+    
+    async execute(interaction)
+    {
+        await interaction.reply('Shut the fuck up');
+    }
+}
---
+++ index.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+
+const {REST} = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent]
+});
+
+//List of Commands
+const commands = [];
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles)
+{
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
+}
+
+client.on("ready", () => {
+    //Get Ids of the servers
+    const guild_ids = client.guilds.cache.map(guild => guild.id);
+
+    const rest = new REST({version: '9'}).setToken(process.env.TOKEN);
+    
+    for (const guildId of guild_ids) 
+    {
+        rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId), 
+            {body: commands})
+        .then(() => console.log('Succesfully updated commands for guild ' + guildId))
+        .catch(console.error);
+    }
+});
+
+client.on("interactionCreate", async interaction => {
+    if(!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if(!command) return;
+
+    try {
+        await command.execute(interaction);
+    }
+    catch(e)
+    {
+        console.error(e);
+        await interaction.reply({content: "There was an error executing the command"});
+    }
+});
+
+client.login(process.env.TOKEN);
