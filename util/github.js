87916ae492dd56d2cc65974a4bad540f829f9c1e
Holden Davis <79615370+holden-davis@users.noreply.github.com>
2023-03-19 19:43:59 +0000

    Changed /server-status to now use Minestat to retrieve info instead of hard-coded values

--- util/github.js
+++ util/github.js
@@ -1,9 +1,9 @@
-async function getLatestCommitMessage() {
+async function getLatestCommit() {
     const url = 'https://api.github.com/repos/Karmetic/jalpha-minecraft-server/commits';
     const response = await fetch(url);
     const commits = await response.json();
-    const latestCommitMessage = commits[0].commit.message;
-    return latestCommitMessage;
+    const commit = commits[0]
+    return commit
 }
 
-module.exports = getLatestCommitMessage;
+module.exports = getLatestCommit;
